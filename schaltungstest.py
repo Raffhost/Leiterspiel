900155fadd797cc1718b6bb902e49461b9bd6aaf
RaffHost Laptop <danil2005895@gmail.com>
2026-03-12 11:59:50 +0100

    Created schaltungstest.py

---
+++ schaltungstest.py
@@ -0,0 +1,24 @@
+from gpiozero import Button, LED
+from signal import pause
+from time import sleep
+
+# LEDs
+led_pins = [17, 27, 22, 23, 24, 25, 5, 6]
+leds = [LED(pin) for pin in led_pins]
+
+# Button
+button = Button(26)
+
+def all_leds_on():
+    
+    for led in leds:
+        led.on()
+
+    sleep(1)
+    
+    for led in leds:
+        led.off()
+
+button.when_pressed = all_leds_on
+
+pause()
