2b2c5424800b64365e4be41a2bb5e77f957fc687
RaffHost Laptop <danil2005895@gmail.com>
2026-03-26 12:28:41 +0100

    Added laddergame.py and config.py

---
+++ main/config.py
@@ -0,0 +1,3 @@
+#LEDs und Button
+LED_PINS = [17, 27, 22, 23, 24, 25, 5, 6]
+BUTTON_PIN = 26
---
+++ main/laddergame.py
@@ -0,0 +1,38 @@
+from time import sleep
+from gpiozero import LED, Button
+from config import LED_PINS, BUTTON_PIN
+
+class Laddergame:
+    def __init__(self, led_pins=LED_PINS, button_pin=BUTTON_PIN):
+        self._current_led_index = 0
+        self._leds = [LED(pin) for pin in led_pins]
+        self._button = Button(button_pin)
+        self.__debounce_ms = 200
+        self._blink_time_s = 1
+
+    def __current_led(self):
+        return self._leds[self._current_led_index]
+
+    def _next_level(self):
+        self._current_led_index += 1
+
+    def _current_led_is_on(self):
+        return self.__current_led().value
+
+    def _flip_current_led(self):
+        if self._current_led_is_on():
+            self.__current_led().off()
+        else:
+            self.__current_led().on()
+
+    def start(self):
+        for i in range(len(self._leds)):
+            self._current_led_index = i
+            self.__current_led().on()
+            sleep(self._blink_time_s)
+            self.__current_led().off()
+
+
+if __name__ == "__main__":
+    game = Laddergame()
+    game.start()
