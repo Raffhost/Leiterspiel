ae9e020f9fa95449f33d3a12b69bddbc2b6f115a
RaffHost Laptop <danil2005895@gmail.com>
2026-05-21 12:15:44 +0200

    Finished v.2

--- main/highscores.py
+++ main/highscores.py
@@ -4,16 +4,16 @@ from config import HIGHSCORE_FILE
 class Highscores:
     def __init__(self, file=HIGHSCORE_FILE):
         self._highscore_file = file
-        self._highscore = []
+        self._highscores = []
 
     def display(self):
         print("Highscores:")
-        for str in self._highscore:
+        for str in self._highscores:
             print(f"{str['name']}: {str['count']}")
 
     def add_highscore(self, name, level):
-        self._highscore.append({"name": name, "count": level})
+        self._highscores.append({"name": name, "count": level})
 
     def save_highscores(self):
         with open(self._highscore_file, "w") as json_file:
-            json.dump(self._highscore, json_file)
+            json.dump(self._highscores, json_file)
